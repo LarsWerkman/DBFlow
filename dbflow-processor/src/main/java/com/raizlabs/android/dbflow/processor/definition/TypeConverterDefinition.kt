b8d5f31a82e6dc4d2101db7b02349a7c94ae1ea1
Lars Werkman <lars@one.fit>
2018-06-04 12:39:38 +0200

    find adapter class even if its not an directsupertype

--- dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/TypeConverterDefinition.kt
+++ dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/TypeConverterDefinition.kt
@@ -10,6 +10,7 @@ import javax.lang.model.element.TypeElement
 import javax.lang.model.type.DeclaredType
 import javax.lang.model.type.MirroredTypesException
 import javax.lang.model.type.TypeMirror
+import javax.tools.Diagnostic
 
 /**
  * Description: Holds data about type converters in order to write them.
@@ -41,22 +42,30 @@ class TypeConverterDefinition(val className: ClassName,
 
         val types = manager.typeUtils
 
-        var typeConverterSuper: DeclaredType? = null
+
         val typeConverter = manager.typeUtils.getDeclaredType(manager.elements
-                .getTypeElement(ClassNames.TYPE_CONVERTER.toString()))
+            .getTypeElement(ClassNames.TYPE_CONVERTER.toString()))
+        var typeConverterSuper: DeclaredType = findSuperType(manager, typeMirror, typeConverter)
 
-        for (superType in types.directSupertypes(typeMirror)) {
-            val erasure = types.erasure(superType)
-            if (types.isAssignable(erasure, typeConverter) || erasure.toString() == typeConverter.toString()) {
-                typeConverterSuper = superType as DeclaredType
-            }
-        }
+        val typeArgs = typeConverterSuper.typeArguments
+        dbTypeName = ClassName.get(typeArgs[0])
+        modelTypeName = ClassName.get(typeArgs[1])
 
-        if (typeConverterSuper != null) {
-            val typeArgs = typeConverterSuper.typeArguments
-            dbTypeName = ClassName.get(typeArgs[0])
-            modelTypeName = ClassName.get(typeArgs[1])
-        }
     }
 
+    private fun findSuperType(manager: ProcessorManager, typeMirror: TypeMirror, typeConverter: DeclaredType): DeclaredType {
+        val types = manager.typeUtils
+        for (superType in types.directSupertypes(typeMirror)) {
+            val erasure = types.erasure(superType)
+            if (types.isSameType(erasure, typeConverter) || erasure.toString() == typeConverter.toString()) {
+                return superType as DeclaredType
+            }
+
+            return findSuperType(manager, superType, typeConverter)
+        }
+
+        manager.messager.printMessage(Diagnostic.Kind.ERROR, "TypeConverters must extend the TypeConverter class",
+            types.asElement(typeMirror))
+        throw UnsupportedOperationException()
+    }
 }
